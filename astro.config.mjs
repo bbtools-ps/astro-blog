55d0cd07e61b89d6b0333e0232a9297a399ffc7e
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2024-11-11 12:39:55 +0100

    site url change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://bbtools-astro-blog.netlify.app",
   integrations: [preact()],
 });
